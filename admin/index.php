48cbe415bf8662b17cddd5ccca261270f88186a4
Shah Alam <shahalam141072@gmail.com>
2023-07-22 01:24:37 +0600

    blog oop project installed

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php
+include_once('components/header.php');
+include_once('components/sidebar.php');
+
+?>
+
+
+<!-- dashboard content start -->
+<?php
+include_once('components/dashboard.php');
+?>
+<!-- dashboard content end -->
+
+
+
+<?php
+include_once('components/footer.php');
+?>
